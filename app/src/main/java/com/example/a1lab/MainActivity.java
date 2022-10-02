c3d0f006e920854f3e45dc69f8c3bed032fabdba
Vovere/Rokas <48134959+voovere@users.noreply.github.com>
2022-10-02 10:58:41 +0300

    New button and textView with function to increment value when the button is clicked

--- app/src/main/java/com/example/a1lab/MainActivity.java
+++ app/src/main/java/com/example/a1lab/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.a1lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button change_count_bttn;
+    TextView count_tv;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count_tv = (TextView) findViewById(R.id.counter_tv);
+        change_count_bttn = (Button) findViewById(R.id.change_count_bttn);
+        change_count_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                count_tv.setText(String.valueOf(count));
+            }
+        });
     }
 }
