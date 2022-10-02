bfd55740009cca74bfdd2f911f98de1089d95889
Vovere/Rokas <48134959+voovere@users.noreply.github.com>
2022-10-02 11:09:19 +0300

    New button to change text color

--- app/src/main/java/com/example/a1lab/MainActivity.java
+++ app/src/main/java/com/example/a1lab/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a1lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button change_count_bttn;
+    Button change_color_bttn;
     TextView count_tv;
     int count = 0;
 
@@ -24,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 count++;
                 count_tv.setText(String.valueOf(count));
+            }
+        });
+        change_color_bttn = (Button) findViewById(R.id.change_color_bttn);
+        change_color_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count_tv.setTextColor(Color.RED);
             }
         });
     }
